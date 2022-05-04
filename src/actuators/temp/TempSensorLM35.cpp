5e1017d3faf3ae800beacfc5b1cea4d649adcaec
Alessandro Zirondelli <alessandr.zirondelli@studio.unibo.it>
2022-05-04 15:15:56 +0200

    update calclation of tmp sensor

--- src/actuators/temp/TempSensorLM35.cpp
+++ src/actuators/temp/TempSensorLM35.cpp
@@ -8,7 +8,8 @@ TempSensorLM35::TempSensorLM35(int p) : pin(p){
   
 float TempSensorLM35::getTemperature(){
   int value = analogRead(pin);
-  float valueInVolt = value*VCC/1023;  
-  float valueInCelsius = valueInVolt/0.01;
+  /*float valueInVolt = value*VCC/1023;  
+  float valueInCelsius = valueInVolt/0.01;*/
+  float valueInCelsius = ((value * 0.00488) - 0.5) / 0.01;
   return valueInCelsius;
 }
