1575725fa547555584990f963c1f672f859660db
Alessandro Zirondelli <alessandr.zirondelli@studio.unibo.it>
2022-05-04 13:30:48 +0200

    Add attribute display in SelfTask

--- src/tasks/SelfTestTask/SelfTask.cpp
+++ src/tasks/SelfTestTask/SelfTask.cpp
@@ -14,6 +14,7 @@
      this -> machine = machine;
      this -> tempSensor = new TempSensorLM35(TMP_PIN);
      this -> servoMotor = new ServoMotorImpl(SERVO_PIN);
+     //this -> display = new Display();
      this -> state = IDLE;
  };
 
@@ -47,6 +48,7 @@
         case ERROR: {
             //print on display LCD "Assistance required";
             //this->machine->setAssistance();
+            //this->display->print("Assistance required");
         }
     }
  };
